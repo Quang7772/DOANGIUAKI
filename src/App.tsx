377bd628793864670e4ac04c2d707414969611d9
QUANGGG <hhminhquang2005@gmail.com>
2025-10-23 02:46:59 +0000

    dongbo

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
-import "./styles.css";
-// @ts-ignore
-import Home from "./Home";
-// @ts-ignore
-import Layout from "./Layout";
-// @ts-ignore
-import Trang1 from "./Trang1";
-// @ts-ignore
-
-import Trang2 from "./Trang2";
-// @ts-ignore
-import Listsanpham from "./Listsanpham";
-// @ts-ignore
-import Chitietsanpham from "./Chitietsanpham";
-
+// src/App.tsx
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
-  //return <Layout />;
+import "./styles.css";
+
+import Home from "./Home";
+import Layout from "./Layout";
+import Trang1 from "./Trang1";
+import Trang2 from "./Trang2";
+import Listsanpham from "./Listsanpham";
+import Chitietsanpham from "./Chitietsanpham";
+
+const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="trang1" element={<Trang1 />} />
+          <Route path="listsanpham" element={<Listsanpham />} />
           <Route path="sanpham/:id" element={<Chitietsanpham />} />
           <Route path="trang2" element={<Trang2 />} />
-          <Route path="listsanpham" element={<Listsanpham />} />
         </Route>
       </Routes>
     </BrowserRouter>
