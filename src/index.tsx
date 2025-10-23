f5496152d5306d48754a277a26cf99dc216311ff
QUANGGG <hhminhquang2005@gmail.com>
2025-10-23 03:46:38 +0000

    dongbo

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// ✅ Import Bootstrap đúng chuẩn
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
